a2ca4df9c385afa1eab0f27be71b26f8c3d69749
sjlevin <sjlevin@users.noreply.github.com>
2020-08-19 04:32:49 +0000

    deploy: 9444d5fad1a012b523cd427eafc3c15d8f133204

---
+++ precache-manifest.f85643f66a304d7f0b78987f033d7298.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4ae244f43068eab02f738cb700e3e2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0dfc8ca3b3b390b62164",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d3043a1dbf1c45b184fb",
+    "url": "/static/js/2.e3ddf240.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e3ddf240.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dfc8ca3b3b390b62164",
+    "url": "/static/js/main.9a2efd37.chunk.js"
+  },
+  {
+    "revision": "8ec82c2de083a9d4af93",
+    "url": "/static/js/runtime-main.46ee282d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
